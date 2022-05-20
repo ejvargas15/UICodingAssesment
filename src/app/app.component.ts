d82e6b5e422fab9bd0ba1df7757ef4a6d8b58aa5
Edward <evargas@icgteam.com>
2022-05-19 22:50:08 -0400

    Commit to correct gramatical error.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(private http: HttpClient) {}
   title = 'api-angular';
+  dataList: any;
+
+  ngOnInit() {
+		// API Call
+		let headers = new HttpHeaders({
+			'x-rapidapi-host': 'random-facts2.p.rapidapi.com',
+			'x-rapidapi-key': 'your-api-key'
+		});
+		this.http
+			.get('https://jsonplaceholder.typicode.com/photos').toPromise().then(data=>{
+        this.dataList = data;
+        console.log(this.dataList);
+      })
+			
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http'; // importing the http module
+import {MatCardModule} from '@angular/material/card';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule, // adding it in the imports
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
